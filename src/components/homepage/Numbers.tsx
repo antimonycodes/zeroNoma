d8302536f26003ff5c0e2f8e28ccc6063dd17482
antimonycodes <akinrinadetobilobasb@gmail.com>
2024-09-02 17:11:45 +0100

    final

--- src/components/homepage/Numbers.tsx
+++ src/components/homepage/Numbers.tsx
@@ -11,7 +11,7 @@ const Numbers = () => {
     <div className='mx-0 my-8 md:mx-16  md:flex  md:items-center md:justify-center'>
       <div className='md:absolute bottom-[-10%] md:px-40 bg-white md:bg-black text-white mx-auto w-full md:w-fit py-4 flex flex-col md:flex-row gap-8 md:gap-24 items-center'>
         {cards.map((card, index) => (
-          <div key={index} className='flex items-center justify-center py-8 bg-black w-full'>
+          <div key={index} className='flex items-center justify-center py-8 md:py-0 bg-black w-full'>
             <div className='flex flex-col items-center gap-2 md:gap-4'>
               <h1 className='text-4xl md:text-6xl'>{card.no}</h1>
               <h2 className='text-lg md:text-xl'>{card.title}</h2>
